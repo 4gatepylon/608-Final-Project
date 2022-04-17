71478715b6b2a3d904743e6181bf1a7162d7ce58
Daniela Velez <danielavelez1201@gmail.com>
2022-04-17 13:12:06 -0400

    Tailwind and finished frontend

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    var response = await fetch(
+      "http://608dev-2.net/sandbox/sc/team10/server.py"
+    );
+    var body = await response.text();
+    setData([...data, body]);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData();
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10">
+      <h1 className="text-lg mb-5 font-bold">Data</h1>
+      <p className="text-lg">{data && <>{data}</>}</p>
     </div>
   );
 }
