c46b5bb8cfcc83b978f75aa2b9c64f232d39125d
Daniela Velez <danielavelez1201@gmail.com>
2022-04-17 13:43:18 -0400

    Plotting

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
 
 function App() {
   const [data, setData] = useState([]);
 
   async function fetchData() {
-    var response = await fetch(
+    let response = await fetch(
       "http://608dev-2.net/sandbox/sc/team10/server.py"
     );
-    var body = await response.text();
-    setData([...data, body]);
+    let body = await response.text();
+    console.log(JSON.parse(body));
+    setData([...data, JSON.parse(body)]);
   }
 
   useEffect(() => {
@@ -21,7 +23,24 @@ function App() {
   return (
     <div className="p-10">
       <h1 className="text-lg mb-5 font-bold">Data</h1>
-      <p className="text-lg">{data && <>{data}</>}</p>
+      <p className="text-lg">
+        {data && (
+          <>
+            <Plot
+              data={[
+                {
+                  x: data.times,
+                  y: data.x,
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: { color: "red" },
+                },
+              ]}
+              layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+            />
+          </>
+        )}
+      </p>
     </div>
   );
 }
