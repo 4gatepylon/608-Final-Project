b7fd2b27623258176e0829eeddb283fd12e179e4
Daniela Velez <danielavelez1201@gmail.com>
2022-04-25 16:59:37 -0400

    Initial map

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,157 +1,242 @@
-import React from "react";
-import Plot from "react-plotly.js";
+/*
+ * Copyright 2021 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-type ServerData = {
-  times: number[];
-  x_x: number[];
-  x_y: number[];
-  v_x: number[];
-  v_y: number[];
-  a_x: number[];
-  a_y: number[];
-  speeds: number[];
-  directions: number[];
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { createCustomEqual } from "fast-equals";
+import { isLatLngLiteral } from "@googlemaps/typescript-guards";
+
+console.log(process.env);
+const render = (status: Status) => {
+  return <h1>{status}</h1>;
 };
 
-function App() {
-  let emptyData: ServerData = {
-    times: [],
-    x_x: [],
-    x_y: [],
-    v_x: [],
-    v_y: [],
-    a_x: [],
-    a_y: [],
-    speeds: [],
-    directions: [],
+const App: React.VFC = () => {
+  const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
+  const [zoom, setZoom] = React.useState(3); // initial zoom
+  const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0,
+  });
+
+  const onClick = (e: google.maps.MapMouseEvent) => {
+    setClicks([...clicks, e.latLng!]);
   };
 
-  const [data, setData] = React.useState(emptyData);
+  const onIdle = (m: google.maps.Map) => {
+    console.log("onIdle");
+    setZoom(m.getZoom()!);
+    setCenter(m.getCenter()!.toJSON());
+  };
 
-  async function fetchData() {
-    let response = await fetch(
-      "http://608dev-2.net/sandbox/sc/team10/server.py"
-    );
-    let body: string = await response.text();
-    let data: ServerData = JSON.parse(body);
-    setData(data);
-  }
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      fetchData();
-    }, 1000);
-    return () => clearInterval(interval);
-  });
-  return (
-    <div className="p-10">
-      <div className="text-xl mb-5 font-bold underline text-slate-800">
-        Data
-      </div>
-      <p className="text-lg">
-        {data && (
-          <div className="flex flex-row flex-wrap">
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["x_x"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "X Position" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["x_y"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "Y Position" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["v_x"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "X Velocity" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["v_y"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "Y Velocity" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["a_x"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "X Acceleration" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["a_y"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "Y Acceleration" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["speeds"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "Speeds" }}
-            />
-            <Plot
-              data={[
-                {
-                  x: data["times"],
-                  y: data["directions"],
-                  type: "scatter",
-                  mode: "lines+markers",
-                  marker: { color: "red" },
-                },
-              ]}
-              layout={{ width: 320, height: 240, title: "Directions" }}
-            />
-          </div>
-        )}
-      </p>
+  const form = (
+    <div
+      style={{
+        padding: "1rem",
+        flexBasis: "250px",
+        height: "100%",
+        overflow: "auto",
+      }}
+    >
+      <label htmlFor="zoom">Zoom</label>
+      <input
+        type="number"
+        id="zoom"
+        name="zoom"
+        value={zoom}
+        onChange={(event) => setZoom(Number(event.target.value))}
+      />
+      <br />
+      <label htmlFor="lat">Latitude</label>
+      <input
+        type="number"
+        id="lat"
+        name="lat"
+        value={center.lat}
+        onChange={(event) =>
+          setCenter({ ...center, lat: Number(event.target.value) })
+        }
+      />
+      <br />
+      <label htmlFor="lng">Longitude</label>
+      <input
+        type="number"
+        id="lng"
+        name="lng"
+        value={center.lng}
+        onChange={(event) =>
+          setCenter({ ...center, lng: Number(event.target.value) })
+        }
+      />
+      <h3>{clicks.length === 0 ? "Click on map to add markers" : "Clicks"}</h3>
+      {clicks.map((latLng, i) => (
+        <pre key={i}>{JSON.stringify(latLng.toJSON(), null, 2)}</pre>
+      ))}
+      <button onClick={() => setClicks([])}>Clear</button>
     </div>
   );
+
+  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) {
+    return <h1>Missing API key</h1>;
+  }
+
+  return (
+    <div style={{ display: "flex", height: "100%" }}>
+      <Wrapper apiKey={apiKey} render={render}>
+        <Map
+          center={center}
+          onClick={onClick}
+          onIdle={onIdle}
+          zoom={zoom}
+          style={{ flexGrow: "1", height: "100%" }}
+        >
+          {clicks.map((latLng, i) => (
+            <Marker key={i} position={latLng} />
+          ))}
+        </Map>
+      </Wrapper>
+      {/* Basic form for controlling center and zoom of map. */}
+      {form}
+    </div>
+  );
+};
+interface MapProps extends google.maps.MapOptions {
+  style: { [key: string]: string };
+  onClick?: (e: google.maps.MapMouseEvent) => void;
+  onIdle?: (map: google.maps.Map) => void;
+  children: React.ReactNode;
 }
+
+const Map: React.FC<MapProps> = ({
+  onClick,
+  onIdle,
+  children,
+  style,
+  ...options
+}) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const [map, setMap] = React.useState<google.maps.Map>();
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+  // because React does not do deep comparisons, a custom hook is used
+  // see discussion in https://github.com/googlemaps/js-samples/issues/946
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options);
+    }
+  }, [map, options]);
+
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) =>
+        google.maps.event.clearListeners(map, eventName)
+      );
+
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
+
+  return (
+    <>
+      <div ref={ref} style={style} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // set the map prop on the child component
+          return React.cloneElement(child, { map });
+        }
+      })}
+    </>
+  );
+};
+
+const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
+  const [marker, setMarker] = React.useState<google.maps.Marker>();
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new google.maps.Marker());
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+
+  return null;
+};
+
+const deepCompareEqualsForMaps = createCustomEqual(
+  (deepEqual) => (a: any, b: any) => {
+    if (
+      isLatLngLiteral(a) ||
+      a instanceof google.maps.LatLng ||
+      isLatLngLiteral(b) ||
+      b instanceof google.maps.LatLng
+    ) {
+      return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
+    }
+
+    // TODO extend to other types
+
+    // use fast-equals for other objects
+    return deepEqual(a, b);
+  }
+);
+
+function useDeepCompareMemoize(value: any) {
+  const ref = React.useRef();
+
+  if (!deepCompareEqualsForMaps(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+function useDeepCompareEffectForMaps(
+  callback: React.EffectCallback,
+  dependencies: any[]
+) {
+  React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  ReactDom.render(<App />, document.getElementById("root"));
+});
 
 export default App;
