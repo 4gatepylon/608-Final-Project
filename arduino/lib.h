8e4df6a7039a0bff20aecfa9883b33261e9639b8
Adriano Hernandez <adrianoh@mit.edul>
2022-04-25 00:33:30 -0400

    magic

--- arduino/lib.h
+++ arduino/lib.h
@@ -16,13 +16,12 @@ enum Direction
     RIGHT
 };
 
-const char PREFIX[] = "{\"wifiAccessPoints\": ["; //beginning of json body
-const char SUFFIX[] = "]}"; //suffix to POST request
-const char API_KEY[] = "AIzaSyAQ9SzqkHhV-Gjv-71LohsypXUH447GWX8"; //don't change this and don't share this
+const char PREFIX[] = "{\"wifiAccessPoints\": [";                 // beginning of json body
+const char SUFFIX[] = "]}";                                       // suffix to POST request
+const char API_KEY[] = "AIzaSyAQ9SzqkHhV-Gjv-71LohsypXUH447GWX8"; // don't change this and don't share this
 
-uint8_t channel = 1; //network channel on 2.4 GHz
-byte bssid[] = {0x04, 0x95, 0xE6, 0xAE, 0xDB, 0x41}; //6 byte MAC address of AP you're targeting.
-
+uint8_t channel = 1;                                 // network channel on 2.4 GHz
+byte bssid[] = {0x04, 0x95, 0xE6, 0xAE, 0xDB, 0x41}; // 6 byte MAC address of AP you're targeting.
 
 esp_now_peer_info_t peerInfo;
 
@@ -30,7 +29,6 @@ struct Vec
 {
     float x;
     float y;
-    
 };
 
 struct WireData
@@ -52,12 +50,11 @@ const uint16_t IN_BUFFER_SIZE = 3500;  // size of buffer to hold HTTP request
 const uint16_t OUT_BUFFER_SIZE = 3500; // size of buffer to hold HTTP response
 const uint16_t JSON_BODY_SIZE = 3000;
 
-char network[] = "EECS_Labs";
+char network[] = "MIT";
 char password[] = "";
 const char user[] = "";
 char request_buffer[IN_BUFFER_SIZE];   // char array buffer to hold HTTP request
 char response_buffer[OUT_BUFFER_SIZE]; // char array buffer to hold HTTP response
-
 
 /*----------------------------------
  * char_append Function:
@@ -69,7 +66,3 @@ char response_buffer[OUT_BUFFER_SIZE]; // char array buffer to hold HTTP respons
  * Return value:
  *    boolean: True if character appended, False if not appended (indicating buffer full)
  */
-
-
-
-
